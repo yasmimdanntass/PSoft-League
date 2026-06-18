c48a90a09057b836fb07bd530cc4f9c55e47e709
Yasmim Dantas <yasmimdantas@MacBook-Air-de-Yasmim.local>
2026-06-17 22:26:09 -0300

    structure: added the spring structure

---
+++ src/main/java/ufcg/psoftleague/PsoftleagueApplication.java
@@ -0,0 +1,13 @@
+package ufcg.psoftleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsoftleagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsoftleagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufcg/psoftleague/PsoftleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package ufcg.psoftleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsoftleagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
